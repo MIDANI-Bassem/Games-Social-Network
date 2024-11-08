6e1e6a7fa6ad814044640931ee4e5bd0df5dc254
MIDANI <32643487+MIDANI-Bassem@users.noreply.github.com>
2024-11-08 10:42:34 +0100

    Configuration SpringBoot Project
    Ajout des dépendences

---
+++ game-loom-api/src/main/java/org/gameloom/connect/game/GameLoomApiApplication.java
@@ -0,0 +1,13 @@
+package org.gameloom.connect.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameLoomApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameLoomApiApplication.class, args);
+	}
+
+}
---
+++ game-loom-api/src/test/java/org/gameloom/connect/game/GameLoomApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gameloom.connect.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameLoomApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
