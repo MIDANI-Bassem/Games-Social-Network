30270e4b0b19fb2d04d2c0b925c1540b99d2f682
MIDANI <32643487+MIDANI-Bassem@users.noreply.github.com>
2024-12-17 16:35:27 +0100

    refactoring angular

--- game-loom-network-ui/game-loom-ui/README.md
+++ game-loom-network-ui/game-loom-ui/README.md
@@ -1,4 +1,4 @@
-# GameLoomNetworkUi
+# GameLoomUi
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.13.
 
--- game-loom-network-ui/game-loom-ui/src/app/app.component.spec.ts
+++ game-loom-network-ui/game-loom-ui/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'game-loom-network-ui'`, () => {
+  it(`should have as title 'game-loom-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('game-loom-network-ui');
+    expect(app.title).toEqual('game-loom-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('game-loom-network-ui app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('game-loom-ui app is running!');
   });
 });
