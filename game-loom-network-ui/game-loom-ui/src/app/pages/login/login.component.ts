45a66bb1f3fb41970297ef5227bc969c4d33bed1
MIDANI <32643487+MIDANI-Bassem@users.noreply.github.com>
2024-12-23 14:54:46 +0100

    feat: adding register page

--- game-loom-network-ui/game-loom-ui/src/app/pages/login/login.component.ts
+++ game-loom-network-ui/game-loom-ui/src/app/pages/login/login.component.ts
@@ -19,37 +19,6 @@ export class LoginComponent {
 
   authRequest: AuthenticationRequest = {email:"", password:""}
   errorMsg:Array<string> = [];
-  /*** 
-  login() {
-    this.errorMsg = [];
-    this.authService.authenticate({
-      body: this.authRequest
-    }).subscribe({
-      next: (response) => {
-        console.log(response);
-        this.tokenService.setToken(response.token as string);
-        this.router.navigate(['dashboard']);
-      },
-      error: (err) => {
-        if (err.error instanceof Blob) {
-          const reader = new FileReader();
-          reader.onload = () => {
-            try {
-              const errorJson = JSON.parse(reader.result as string);
-              console.error(errorJson);
-              this.errorMsg = errorJson.validationErrors || [];
-            } catch (e) {
-              console.error('Erreur lors du parsing du Blob:', e);
-            }
-          };
-          reader.readAsText(err.error); // Convertir le Blob en texte
-        } else {
-          console.error(err.error);
-        }
-      }
-    });
-  }
-    */
   login() {
     this.errorMsg = [];
     this.authService.authenticate({
---
+++ game-loom-network-ui/game-loom-ui/src/app/pages/register/register.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RegistrationRequest } from 'src/app/services/models';
+import { AuthenticationService } from 'src/app/services/services';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent  {
+
+  registerRequest: RegistrationRequest = {
+    email: '',
+    firstName: '',
+    lastName: '',
+    password: ''
+  }
+  errorMsg: Array<string> = [];
+
+  constructor(
+    private router:Router,
+    private authService: AuthenticationService
+
+  ) { }
+
+
+  login() {
+    this.router.navigate(['login']);
+  }
+  register() {
+    this.errorMsg = [];
+    this.authService.register({
+      body: this.registerRequest
+    }).subscribe({
+      next: (response) => {
+        this.router.navigate(['activate-aacount']);
+      },
+      error: (error) => {
+        this.errorMsg = error.error.validationErrors;
+      }
+    });
+  }
+}
